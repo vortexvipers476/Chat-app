42a7191e7b615038cb6df771cebf2c21106131c7
vortexvipers476 <vortexvipers1003@gmail.com>
2025-09-22 11:14:09 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        gray: {
-          900: '#111827',
-          800: '#1f2937',
-          700: '#374151',
-        },
-      },
-    },
+    extend: {},
   },
-  darkMode: 'class',
-};
+  plugins: [],
+}
